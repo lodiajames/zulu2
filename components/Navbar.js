967c01e93f70449e5cfe1c04abe5702b3e92ab6a
ibrahima <lodiajames@hotmail.com>
2023-02-04 22:01:38 -0500

    added loading effect and error

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,6 +6,7 @@ export default function Navbar() {
       <div className=''>
          <Header/>
          <Banner/>
+         
          </div>
     
   )
---
+++ src/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center items-center py-5'>
+        <img src='./loading.svg ' className='h-20' alt='loading icon'/>
+    </div>
+  )
+}
