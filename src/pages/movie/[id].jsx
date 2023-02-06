8947f304281508dd8a9c7a229e26111918c0f3f1
ibrahima <lodiajames@hotmail.com>
2023-02-05 20:10:15 -0500

    styled dynamic movie

--- src/pages/movie/[id].jsx
+++ src/pages/movie/[id].jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image';
 
 
 export async function getServerSideProps(context){
@@ -17,7 +18,24 @@ export async function getServerSideProps(context){
 export default function Movie({data}){
    
  return <div className='w-full'>
-       <div className='p-4 m '></div>
+       <div className='p-4 mt-8 flex flex-col md:flex-row items-center content-center max-6xl mx-auto mb:space-x-6'>
+
+       <Image src={`https://image.tmdb.org/t/p/original/${data.backdrop_path || data.poster_path} `} width={500} height={300}
+        
+        className="rounded-lg"
+        placeholder='blur' blurDataURL='/loading.svg' alt='movie pic' style={{
+            maxWidth:"100%", height:"100%"
+        }}>
+
+        </Image>
+        <div className='p-2'>
+              <h2 className='text-lg mb-3 font-bold text-center'>{data.title || data.name} </h2>
+              <p className='text-lg mb-2'><span className='font-semibold mr-2 text-red-200'>Summary:</span>{data.overview}</p>
+              <p className='text-lg font-bold'><span  className='font-semibold mr-1 text-red-200'>Released Date:</span>{data.release_date || data.first_air_date}</p>
+              <p className='text-lg font-bold'><span  className='font-semibold mr-1 text-red-200'>Genres:</span>{data.genres.map(genre=> genre.name)}</p>
+
+          </div>
+       </div>
 
  </div>
 }
